a1dd4f2201c49719950231f7a5a1631ca53cd4a1
Mikael Færøvik <mikael.farovik@gmail.com>
2026-02-23 12:00:32 +0100

    more keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,9 +9,21 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- Manage windows
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Open vertical split" })
-keymap.set("n", "<leader>sh", "<C-w>h", { desc = "Open horizontal split" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Open horizontal split" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Equalize splits" })
 keymap.set("n", "<leader>sc", "<cmd>close<CR>", { desc = "Close split" })
+
+-- Resize splits
+keymap.set("n", "<A-k>", "<cmd>resize +2<CR>", { desc = "Make split wider" })
+keymap.set("n", "<A-j>", "<cmd>resize -2<CR>", { desc = "Make split narrower" })
+keymap.set("n", "<A-h>", "<cmd>vertical resize -2<CR>", { desc = "Make split taller" })
+keymap.set("n", "<A-l>", "<cmd>vertical resize +2<CR>", { desc = "Make split shorter" })
+
+-- Move focus
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top split" })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
 
 -- Manage tabs
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
