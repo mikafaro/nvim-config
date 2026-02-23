59662b2b23cd16e9e6f07ba448906307ce65e33f
Mikael Færøvik <mikael.farovik@gmail.com>
2026-02-23 11:29:06 +0100

    added keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 local keymap = vim.keymap
 
 vim.g.mapleader = " "
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+
+-- Manage windows
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Open vertical split" })
+keymap.set("n", "<leader>sh", "<C-w>h", { desc = "Open horizontal split" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Equalize splits" })
+keymap.set("n", "<leader>sc", "<cmd>close<CR>", { desc = "Close split" })
+
+-- Manage tabs
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) 
