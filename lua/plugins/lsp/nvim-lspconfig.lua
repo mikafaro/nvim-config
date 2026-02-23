94c50a722e8ed9d0f349cd9b7498a5c99d7af573
Mikael Færøvik <mikael.farovik@gmail.com>
2026-02-23 14:17:55 +0100

    fix lua_ls library

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -9,7 +9,7 @@ return {
             globals = { 'vim' },
           },
           workspace = {
-            library = vim.api.nvim_get_runtime_file("", true),
+            library = vim.env.VIMRUNTIME,
             checkThirdParty = false,
           },
           telemetry = {
