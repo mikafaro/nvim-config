1f53b9419e045e057f6a4d89f6419d1ecc4a2ef8
Mikael Færøvik <mikael.farovik@gmail.com>
2026-04-09 11:21:00 +0200

    added roslyn.nvim

---
+++ lua/plugins/roslyn.lua
@@ -0,0 +1,13 @@
+return {
+  "seblyng/roslyn.nvim",
+  ---@module 'roslyn.config'
+  ---@type RoslynNvimConfig
+  opts = {
+    filewatching = "auto",
+    choose_target = nil,
+    ignore_target = nil,
+    broad_search = false,
+    lock_target = false,
+    silent = false,
+  },
+}
